345515398ea63842bfc59bbc490b085e8f925d94
muxing37 <z1099511627776@outlook.com>
2026-06-10 21:07:12 +0800

    调整代码结构

---
+++ src/main_client.cpp
@@ -0,0 +1,6 @@
+#include "ctrl.h"
+
+int main(int argc,char **argv) {
+    start_client();
+    return 0;
+}
---
+++ src/main_server.cpp
@@ -0,0 +1,6 @@
+#include "ctrl.h"
+
+int main(int argc,char **argv) {
+    start_server();
+    return 0;
+}
